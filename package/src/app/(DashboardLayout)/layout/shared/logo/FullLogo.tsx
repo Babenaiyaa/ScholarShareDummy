55dff77ade6a04589512058477222ba7a7ed537c
sinthujan23 <sinthusives@gmail.com>
2025-02-19 04:26:32 +0530

    dashboard

---
+++ package/src/app/(DashboardLayout)/layout/shared/logo/FullLogo.tsx
@@ -0,0 +1,13 @@
+"use client";
+import React from "react";
+import Image from "next/image";
+import Logo from "/public/images/logos/dark-logo.svg";
+import Logowhite from "/public/images/logos/light-logo.svg";
+import Link from "next/link";
+const FullLogo = () => {
+  return (
+    <h1>ScholarShare</h1>
+  );
+};
+
+export default FullLogo;
